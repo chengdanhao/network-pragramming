aa953d9a7af4512e4a7e04efd3f0e377d8e04d72
daniel <1007381610@qq.com>
2016-02-26 22:03:33 +0800

    添加代码

--- C/select_server.c
+++ C/select_server.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -11,28 +11,33 @@
 #define BUF_SIZE 256
 #define PORT 10001
 
-#define BACKLOG 10
-#define CLIENTS_NUM 30
+#define BACKLOG 5
+#define MAX_CLIENTS_NUM 3
 
 #define ACCEPT_MSG "Connection Established!"
 #define REFUSE_MSG "Refuse, connection numbers reach the upper limit!"
 
 int main(int argc, char* argv[]) {
-	ssize_t nread;
-	size_t nwrite;
+	ssize_t nrecv;
+	size_t nsend;
 	socklen_t cli_len;
 	char recv_buf[BUF_SIZE] = {'\0'};
 	char send_buf[BUF_SIZE] = {'\0'};
-	int i, cur_conn_cnt, master_sock, new_sock;
-	int sd, max_sd, cli_socks[CLIENTS_NUM];
+	int i, cur_conn_cnt = 0, master_sock, new_sock;
+	int sd, max_sd, cli_socks[MAX_CLIENTS_NUM] = {0};
 	struct sockaddr_in serv_addr, cli_addr;
-	struct fd_set rfds;
-	struct fd_set wfds;
-	struct fd_set efds;
+	int opt;
+	fd_set rfds;
 
 	master_sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (master_sock < 0) {
-		perror("socket");
+		perror("ERROR socket");
+		exit(-1);
+	}
+
+	// just a good habit
+	if (setsockopt(master_sock, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 ) {
+		perror("ERROR setsockopt");
 		exit(-1);
 	}
 
@@ -42,24 +47,23 @@ int main(int argc, char* argv[]) {
 	serv_addr.sin_port = htons(PORT);
 
 	if (bind(master_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) {
-		perror("bind");
+		perror("ERROR bind");
 		exit(-1);
 	}
 
 	if (listen(master_sock, BACKLOG)) {
-		perror("listen");
+		perror("ERROR listen");
 		exit(-1);
 	}
 
-	cli_len = sizeof(cli_addr);
 	while(1) {
 		FD_ZERO(&rfds);
 		FD_SET(master_sock, &rfds);
 
-		maxfd = master_sock;
+		max_sd = master_sock;
 
 		// add child sockets to set
-		for (i = 0; i < CLIENTS_NUM; i++) {
+		for (i = 0; i < MAX_CLIENTS_NUM; i++) {
 			sd = cli_socks[i];
 
 			if (sd > 0) {
@@ -75,10 +79,12 @@ int main(int argc, char* argv[]) {
 		 * > 0  : Success, the number of file descriptors contained in the three descriptor sets.
 		 * = 0  : the timeout expires before anything interesting happens.  
 		 * = -1 : errno is set to indicate the error; the file descriptor sets are unmodified, and timeout becomes undefined.
+		 * 
+		 * the 1st argument can simply set to FD_SETSIZE
 		 */
 		switch (select(max_sd + 1, &rfds, NULL, NULL, NULL)) {
 			case -1:
-				perror("select");
+				perror("ERROR select");
 				exit(-1);
 			case 0:
 				// timeout, just loop again
@@ -86,34 +92,42 @@ int main(int argc, char* argv[]) {
 			default:
 				// new connection comes.
 				if (FD_ISSET(master_sock, &rfds)) {
+					cli_len = sizeof(cli_addr);
 					new_sock = accept(master_sock, (struct sockaddr*)&cli_addr, &cli_len);
 					if (new_sock < 0) {
-						perror("accept");
+						perror("ERROR accept");
 						exit(-1);
 					}
 
 					// Do connection numbers reach the upper limit?
-					if (cur_conn_cnt > CLIENTS_NUM - 1) {
+					if (cur_conn_cnt > MAX_CLIENTS_NUM - 1) {
 						printf(REFUSE_MSG);
 
-						if (send(new_sock, REFUSE_MSG, strlen(REFUSE_MSG), 0)) {
-							perror("send refuse message");
+						if (send(new_sock, REFUSE_MSG, strlen(REFUSE_MSG), 0) < 0) {
+							perror("ERROR send refuse message");
 						}
 
-						printf(REFUSE_MSG);
 						close(new_sock);
 						continue;
 					}
 
 					// not reach the upper limit, accept the socket.
-					printf("new connection, sockfd = %d, ip = %d, port = %d.\n", 
-							new_sock, inet_ntop(cli_addr.sin_addr), ntohl(cli_addr.sin_port));
+					printf("new connection, sockfd = %d, ip = %s, port = %d.\n", 
+							new_sock, inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
 
-					if (send(new_sock, ACCEPT_MSG, strlen(ACCEPT_MSG), 0)) {
-						perror("send accept message");
+					if (send(new_sock, ACCEPT_MSG, strlen(ACCEPT_MSG), 0) < 0) {
+						perror("ERROR send accept message");
 					}
 
 					FD_SET(new_sock, &rfds);
+
+					for (i = 0; i < MAX_CLIENTS_NUM; i++) {
+						if (0 == cli_socks[i]) {
+							cli_socks[i] = new_sock;
+							printf("add sock %d to cli_socks[%d].\n", new_sock, i);
+							break;
+						}
+					}
 
 					// current connection count
 					cur_conn_cnt++;
@@ -125,18 +139,36 @@ int main(int argc, char* argv[]) {
 				}
 
 				// client sockets
-				for (i = 0; i < cur_conn_cnt; i++) {
+				for (i = 0; i < MAX_CLIENTS_NUM; i++) {
 					sd = cli_socks[i];
 
 					if (0 == FD_ISSET(sd, &rfds)) {
 						continue;
 					}
 
-					if (recv(sd, recv_buf, sizeof(recv_buf) - 1, 0) <= 0) {
-						perror("recv");
-						FD_CLR(sd, &rfds);
-						close(sd);
-						cli_socks[i] = 0;
+					nrecv = recv(sd, recv_buf, sizeof(recv_buf) - 1, 0);
+					switch (nrecv) {
+						case -1:
+							perror("ERROR recv message");
+							FD_CLR(sd, &rfds);
+							close(sd);
+							cli_socks[i] = 0;
+							break;
+						case 0:
+							printf("sock %d close the connection.", sd);
+							FD_CLR(sd, &rfds);
+							close(sd);
+							cli_socks[i] = 0;
+							break;
+						default:
+							printf("recv (%ld) message: %s.\n", nrecv, recv_buf);
+							break;
+					}
+
+					strcpy(send_buf, recv_buf);
+
+					if (send(sd, send_buf, strlen(send_buf), 0) < 0) {
+						perror("ERROR echo back");
 					}
 
 				}
