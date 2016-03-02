168f9dbabdbb20a2fbea83de77593177a6d21b53
daniel <1007381610@qq.com>
2016-03-02 20:57:16 +0800

    调试代码

--- C/basic_udp_server.c
+++ C/basic_udp_server.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -10,15 +11,20 @@
 
 void dg_echo(int sockfd, struct sockaddr* cliaddr, socklen_t clilen)
 {
+	socklen_t len;
+	int n;
 	char recv_msg[BUF_SIZE] = {'\0'};
 	char send_msg[BUF_SIZE] = {'\0'};
 
 	while (1) {
 		len = clilen;	// 每次都要更新
-		bzero(recv_msg, sizeof(recv_msg));
-		bzero(send_msg, sizeof(send_msg));
+		bzero((void *)recv_msg, sizeof(recv_msg));
+		bzero((void *)send_msg, sizeof(send_msg));
 
 		n = recvfrom(sockfd, recv_msg, sizeof(recv_msg), 0, cliaddr, &len);
+		// printf("[server] recv msg from %s, msg = %s.\n", inet_ntoa(((struct sockaddr_in*)cliaddr)->sin_addr), recv_msg);
+		printf("[server] recv_msg = %s.\n", recv_msg);
+
 		if (n < 0) {
 			perror("recvfrom");
 			close(sockfd);
@@ -27,7 +33,11 @@ void dg_echo(int sockfd, struct sockaddr* cliaddr, socklen_t clilen)
 
 		// 如果n=0如果做判断？EOF还是长度为0的包？
 
-		n = sendto(sockfd, send_msg, n, 0, cliaddr, &len);
+		sprintf(send_msg, "ECHO BACK : %s", recv_msg);
+
+		// 最后一个参数不是指针
+		n = sendto(sockfd, send_msg, strlen(send_msg), 0, cliaddr, len);
+		printf("[server] send_msg = %s.\n", send_msg);
 	}
 }
 
@@ -40,7 +50,7 @@ int main(int argc, char* argv[]) {
 		exit(-1);
 	}
 
-	bzero(serv_addr, sizeof(serv_addr));
+	bzero((void *)&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(PORT);
