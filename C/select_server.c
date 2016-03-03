a535bb375bc82de83f981641274971c4cfaf238d
daniel <1007381610@qq.com>
2016-03-03 23:00:43 +0800

    修复bzero参数错误问题

--- C/select_server.c
+++ C/select_server.c
@@ -42,7 +42,7 @@ int main(int argc, char* argv[]) {
 		exit(-1);
 	}
 
-	bzero((char*)&serv_addr, 0);
+	bzero((char*)&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(PORT);
