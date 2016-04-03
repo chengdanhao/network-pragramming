1b3c1f9efd534cb9a5b98b3265abd2aa6c8b7e5b
daniel <1007381610@qq.com>
2016-04-03 15:26:49 +0800

    recvfrom timeout

--- C/advio.c
+++ C/advio.c
@@ -6,12 +6,17 @@
 #include <signal.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <sys/select.h>
 #include <arpa/inet.h>
 #include <time.h>
 
+#define BUF_SIZE 256
+
+typedef void (*sighandler_t)(int);
+
 void connect_alarm(int signo)
 {
-	printf("connect timeout.\n");
+	printf("connect_alarm.\n");
 	return;
 }
 
@@ -32,7 +37,6 @@ int connect_timeout(int sockfd, struct sockaddr* sa, socklen_t len, int nsec) {
 	 */
 	if (0 != alarm(nsec)) {
 		printf("alarm was already set.\n");
-		exit(EXIT_FAILURE);
 	}
 	
 	if ((n = connect(sockfd, sa, len)) < 0) {
@@ -47,3 +51,81 @@ int connect_timeout(int sockfd, struct sockaddr* sa, socklen_t len, int nsec) {
 
 	return n;
 }
+
+void sig_alrm(int signo) {
+	printf("sig_alrm.\n");
+	return;
+}
+
+int readable_timeout(int fd, int sec) {
+	fd_set rset;
+	struct timeval tv;
+
+	FD_ZERO(&rset);
+	FD_SET(fd, &rset);
+
+	tv.tv_sec = sec;
+	tv.tv_usec = 0;
+
+	/*
+	 *  -1 : error
+	 * = 0 : timeout
+	 * > 0 : descriptor is readable
+	 */
+	return (select(fd + 1, &rset, NULL, NULL, &tv));
+}
+
+void dg_cli(FILE *fp, int sockfd, struct sockaddr* sa, socklen_t len  ) {
+	int n;
+	char sendline[BUF_SIZE + 1] = {'\0'};
+	char recvline[BUF_SIZE + 1] = {'\0'};
+#ifdef USE_SO_RECVTIMEO
+	const int opt;
+	struct timeval tv;
+
+	tv.tv_sec = 5;
+	tv.tv_usec = 0;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_RECVTIMEO, (void*)&opt,sizeof(opt)) < 0) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+#endif
+
+	while (fgets(sendline, sizeof(sendline), fp) < 0) {
+		sendto(sockfd, sendline, sizeof(sendline), 0, sa, len);
+
+#ifdef USE_SIGALRM
+		alarm(5);
+		if ((n == recvfrom(sockfd, recvfrom, BUF_SIZE, 0, NULL, NULL)) < 0) {
+			if (EINTR == errno) {
+				fprintf(strerr, "socket timeout\n");
+			} else {
+				perror("recvfrom");
+			}
+		} else {
+			alarm(0);	// turn off the alarm
+			recvfrom[n] = '\0';
+			fputs(recvline, fp);
+		}
+#elif USE_SELECT
+		if (readable_timeout(sockfd, 5) == 0) {
+			fprintf(stderr, "socket timeout\n");
+		} else {
+			n = recvfrom(sockfd, recvfrom, BUF_SIZE, 0, NULL, NULL);
+			recvline[n] = '\0';
+			fputs(recvline, stdout);
+		}
+#elif USE_SO_RECVTIMEO
+		if ((n == recvfrom(sockfd, recvfrom, BUF_SIZE, 0, NULL, NULL)) < 0) {
+			if (EWOULDBLOCK == errno) {
+				fprintf(stderr, "socket timeout\n");
+				continue;
+			} else {
+				perror("recvform");
+			}
+
+		}
+#endif
+	}
+}
+
